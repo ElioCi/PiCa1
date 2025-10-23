9d29178f15c8de50253d576d487e3c9a378d3469
ElioCi <107808771+ElioCi@users.noreply.github.com>
2025-10-23 16:10:43 +0200

    Add JWT token verification and update session state

--- PiCa.py
+++ PiCa.py
@@ -1,12 +1,66 @@
 import streamlit as st
 import json
 
-st.set_page_config(page_title="Pica01 App", page_icon="📊")
+from urllib.parse import urlparse, parse_qs
+import requests
+import jwt
+from jwt import PyJWTError
+import time
     
 if 'prot' not in st.session_state or not st.session_state.prot:
     st.session_state.prot = False
+
+# Chiave segreta utilizzata per firmare il token
+SECRET_KEY = 'EC1'
+
+# Funzione per verificare il token
+def verify_token(token):
+    try:
+        # Decodifica e verifica il token
+        decoded_token = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return decoded_token
+    except PyJWTError as e:
+        # Token non valido o scaduto
+        st.error("Token invalid or expired: " + str(e))
+        return None
+
+# Acquisisci il token dai parametri dell'URL
+# query_params = st.experimental_get_query_params()
+query_params = st.query_params
+
+#token = query_params.get("token", [None])[0]
+token = query_params.get("token", [None])
+
+
+if token:
+    # Verifica il token
+    decoded_token = verify_token(token)
+    if decoded_token:
+        st.success("Authorized access!")
+        st.write("Token decoded:", decoded_token)
+        st.session_state.prot = True
+        with open("prot_status.json", "w") as file:
+            json.dump({"prot": st.session_state.prot}, file)
+            
+        # Inserisci qui il codice dell'applicazione Streamlit
+    else:
+        st.error("Access denied: token invalid or expired.")
+        st.session_state.prot = False
+        with open("prot_status.json", "w") as file:
+            json.dump({"prot": st.session_state.prot}, file)
+        st.stop()
+else:
+    st.error("No token provided, access denied.")
+    with open("prot_status.json", "w") as file:
+            json.dump({"prot": st.session_state.prot}, file)
+    st.stop()
+
+
+
+
+st.set_page_config(page_title="Pica01 App", page_icon="📊")
     
-  
+ 
 st.markdown("---")
 st.markdown("<h1 style='text-align: center;'>⭕ PiCA - Piping Calculation</h1>", unsafe_allow_html=True)
 st.markdown("---")
@@ -60,3 +114,4 @@ button_html = """
 st.markdown(button_html, unsafe_allow_html=True)
  
 print ('prot', st.session_state.prot)
+
